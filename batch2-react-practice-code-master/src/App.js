8a46446d1f9d65f71d900d78c1857db30cfd88b2
Khuram <mrkhuram00@gmail.com>
2019-02-13 01:52:01 +0500

    '11/4/2018'

---
+++ batch2-react-practice-code-master/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+
+    // let number = 5;
+    // const name = "My name is Umar";
+
+    let names = ["Umar", "Umar", "Ahmad", "Ali"]
+
+    return (
+      <div>
+        <div style={{ backgroundColor: "red", fontSize: "60px" }} >{ 2 + 2 }</div>
+          <ul className="mylist">
+
+            <li> {names[2]}</li>
+
+          
+            {names.map(function(value, index){
+
+            return <li key={index}>{index}) {value}</li>
+
+          })}
+
+        </ul>
+      </div>
+    );
+
+  }
+
+}
+
+export default App;
